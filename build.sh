3e227ff35c728fd720de00a1bb0f6cc2fe5616a9
FJTY <t01_yamaguchi@jp.fujitsu.com>
2015-03-12 17:37:18 +0900

    Add rm_startup.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ mvn install:install-file -Dfile=./lib/org.restlet.jar -DgroupId=org.restlet -Dar
 mvn install:install-file -Dfile=./lib/org.restlet.ext.json.jar -DgroupId=org.restlet.ext.json -DartifactId=restlet.ext.json -Dversion=2.1.3 -Dpackaging=jar -DgeneratePom=true
 mvn install:install-file -Dfile=./lib/odenos-1.0.0.jar -DgroupId=odenos -DartifactId=odenos -Dversion=1.0.0 -Dpackaging=jar -DgeneratePom=true
 chmod +x ./ocnrm_mn
+chmod +x ./rm_startup.sh
 sleep 1
 mvn package
